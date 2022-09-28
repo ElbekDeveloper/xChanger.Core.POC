926ea71269dcd20922870774fd9c85f0df523b28
Elbek <e.normurodov@mail.ru>
2022-09-27 20:00:54 -0700

    FOUNDATIONS: Retrieve All External Persons

---
+++ xChanger.Core.POC/Services/Foundations/ExternalPersons/ExternalPersonService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using xChanger.Core.POC.Brokers.Sheets;
+using xChanger.Core.POC.Models.Foundations.ExternalPersons;
+
+namespace xChanger.Core.POC.Services.Foundations.ExternalPersons
+{
+    public class ExternalPersonService : IExternalPersonService
+    {
+        private readonly ISheetBroker sheetBroker;
+
+        public ExternalPersonService(ISheetBroker sheetBroker) =>
+            this.sheetBroker = sheetBroker;
+
+        public async ValueTask<List<ExternalPerson>> RetrieveAllExternalPersonsAsync() =>
+            await this.sheetBroker.ReadAllExternalPersonsAsync();
+    }
+}
