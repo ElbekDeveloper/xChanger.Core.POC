2693314541bbca0129a68766418497d09868d735
Elbek <e.normurodov@mail.ru>
2022-09-27 21:42:08 -0700

    CODE RUB: Refactor Controller

--- xChanger.Core.POC/Controllers/PersonsController.cs
+++ xChanger.Core.POC/Controllers/PersonsController.cs
@@ -2,10 +2,8 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RESTFulSense.Controllers;
-using xChanger.Core.POC.Models.Foundations.ExternalPersons;
 using xChanger.Core.POC.Models.Orchestrations.PersonPets;
-using xChanger.Core.POC.Services.Orchestrations.PersonPets;
-using xChanger.Core.POC.Services.Processings.ExternalPersons;
+using xChanger.Core.POC.Services.Coordinations;
 
 namespace xChanger.Core.POC.Controllers
 {
@@ -13,23 +11,13 @@ namespace xChanger.Core.POC.Controllers
     [Route("api/[controller]")]
     public class PersonsController : RESTFulController
     {
-        private readonly IPersonPetOrchestrationService personPetOrchestrationService;
-        private readonly IExternalPersonProcessingService externalPersonProcessingService;
+        private readonly IExternalPersonWithPetsCoordinationService externalPersonWithPetsCoordinationService;
 
-        public PersonsController(
-            IPersonPetOrchestrationService personPetOrchestrationService,
-            IExternalPersonProcessingService externalPersonProcessingService)
-        {
-            this.personPetOrchestrationService = personPetOrchestrationService;
-            this.externalPersonProcessingService = externalPersonProcessingService;
-        }
-
-        [HttpPost]
-        public async ValueTask<ActionResult<PersonPet>> PostPersonWithPetsAsync(PersonPet personPet) =>
-           await this.personPetOrchestrationService.ProcessPersonWithPetsAsync(personPet);
+        public PersonsController(IExternalPersonWithPetsCoordinationService externalPersonWithPetsCoordinationService) =>
+            this.externalPersonWithPetsCoordinationService = externalPersonWithPetsCoordinationService;
 
         [HttpGet]
-        public async ValueTask<ActionResult<List<ExternalPerson>>> GetFormattedExternalPersons() =>
-            await this.externalPersonProcessingService.RetrieveFormattedExternalPersonsAsync();
+        public async ValueTask<ActionResult<List<PersonPet>>> GetStoredPersons() =>
+            await this.externalPersonWithPetsCoordinationService.CoordinateExternalPersons();
     }
 }
