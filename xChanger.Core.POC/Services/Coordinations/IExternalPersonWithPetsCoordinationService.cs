3b2015273099185060e5e7e931b60776f647eeb7
Elbek <e.normurodov@mail.ru>
2022-09-27 21:18:33 -0700

    COORDINATIONS: Coordinate External Persons

---
+++ xChanger.Core.POC/Services/Coordinations/IExternalPersonWithPetsCoordinationService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using xChanger.Core.POC.Models.Foundations.ExternalPersons;
+using xChanger.Core.POC.Models.Foundations.Persons;
+using xChanger.Core.POC.Models.Orchestrations.PersonPets;
+
+namespace xChanger.Core.POC.Services.Coordinations
+{
+    public interface IExternalPersonWithPetsCoordinationService
+    {
+        ValueTask<List<PersonPet>> CoordinateExternalPersons();
+    }
+}
